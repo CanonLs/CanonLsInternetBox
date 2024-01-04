57934ce17a0581608fa723220ac5b51562bae1b2
CanonLs <467706369@qq.com>
2024-01-04 17:13:00 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,20 +7,15 @@ function App() {
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                李潇潇
             </div>
-            <h1>Vite + React</h1>
+            <h1>♥️</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                    好运+ {count}
                 </button>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    点上边的好运
                 </p>
             </div>
             <p className="read-the-docs">
