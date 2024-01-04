a065388f19f1a583932f4e71dc4d5e1b61b9e38e
CanonLs <467706369@qq.com>
2024-01-04 19:23:38 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
     const [count, setCount] = useState(0)
+    const heartDom = useRef<HTMLInputElement>(null)
+
+
+    const addCount = () => {
+        setCount((count) => count + 1)
+        bigHeart()
+    }
+    const bigHeart = () => {
+        // 这里应该调用一个方法来改变样式
+        //点击后，让心变大然后马上缩小
+        // Document.querySelector('.heart').classList.add('big')
+        // setTimeout(() => {
+        //     document.querySelector('.heart').classList.remove('big')
+        // }, 1000)
+        //给heartDom添加class
+        heartDom.current?.classList.add('big')
+        setTimeout(() => {
+            heartDom.current?.classList.remove('big')
+        }, 100)
+    }
+
 
     return (
         <>
             <div>
                 李潇潇
             </div>
-            <h1>♥️</h1>
+            <h1 className="heart" ref={heartDom}>♥️</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => addCount()}>
                     好运+ {count}
                 </button>
                 <p>
